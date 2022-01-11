b61250dca01e8d44a0801b7e0cffd22e22ca3308
anasahmad2233488 <mhd.anas.ahmad@gmail.com>
2022-01-12 05:08:25 +0800

    second commit

--- dashboard/vue.config.js
+++ dashboard/vue.config.js
@@ -20,7 +20,7 @@ module.exports = {
 	],
 	  chainWebpack: config => {
 		config.plugins.delete('html')
-		//config.plugins.delete('preload')
-		//config.plugins.delete('prefetch')
+		config.plugins.delete('preload')
+		config.plugins.delete('prefetch')
 	  }
 };
